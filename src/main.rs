9908b1c965dc17c06f7238f27aef95a738f3f218
Clint.Network <me@clint.network>
2020-05-30 17:11:51 +0200

    Finish

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
-extern crate iron;
+#[macro_use]
+extern crate nickel;
+extern crate serde;
+extern crate serde_json;
+extern crate serde_derive;
 
-use iron::prelude::*;
-use iron::status;
+use std::env;
+use nickel::{Nickel, HttpRouter, MediaType};
 
 fn main() {
-    fn hello_world(request: &mut Request) -> IronResult<Response> {
-        Ok(Response::with((status::Ok, format!("{:#?}", request))))
-    }
+    let mut server = Nickel::new();
+    server.get("/ip", middleware!{ |req, mut res|
+        res.set(MediaType::Json);
+        format!("{}", req.origin.remote_addr)
+    });
+    server.get("/headers", middleware!{ |req, mut res|
+        res.set(MediaType::Json);
+        format!("{:#?}", req.origin.headers)
+    });
+    server.get("/user-agent", middleware!{ |req, mut res|
+        res.set(MediaType::Json);
+        format!("{:#?}", req.origin.headers)
+    });
 
-    let _server = Iron::new(hello_world)
-        .http("localhost:3000")
-        .unwrap();
-
-    println!("On 3000");
+    let port = env::var("PORT").expect("Unable to run it on Heroku.");
+    server.listen(format!("127.0.0.1:{}", port)).expect("Unable to launch hpro.xyz.");
 }
