958a649b09723c76abced626e843c4b7ce698051
Clint.Network <me@clint.network>
2020-12-26 19:21:23 +0100

    Add about endpoint

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,9 @@ extern crate serde_derive;
 
 use std::env;
 use nickel::{Nickel, HttpRouter, MediaType};
+use nickel::extensions::Redirect;
+use nickel::hyper::header::Location;
+use nickel::hyper::status::StatusCode;
 
 fn main() {
     let mut server = Nickel::new();
@@ -20,6 +23,11 @@ fn main() {
     server.get("/user-agent", middleware!{ |req, mut res|
         res.set(MediaType::Json);
         format!("{:#?}", req.origin.headers)
+    });
+    server.get("/about", middleware!{ |_, mut res|
+        res.set(StatusCode::TemporaryRedirect)
+            .set(Location("http://hypeproxy.io/".into()));
+        ""
     });
 
     let port = env::var("PORT").expect("Unable to run it on Heroku.");
