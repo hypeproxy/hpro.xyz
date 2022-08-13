8aefe5315f05d8eb934a3bab250d7a03a0ce8697
Clint.Network <me@clint.network>
2022-08-13 16:12:38 +0200

    Add rewrite

--- next.config.js
+++ next.config.js
@@ -1,36 +1,35 @@
-/** @type {import('next').NextConfig} */
+/** @type {{rewrites(): Promise<{beforeFiles: [{destination: string, source: string}]}>, reactStrictMode: boolean, swcMinify: boolean}} */
 const nextConfig = {
 	reactStrictMode: true,
-	// async rewrites() {
-	// 	return {
-	// 		beforeFiles: [
-	// 			// These rewrites are checked after headers/redirects
-	// 			// and before all files including _next/public files which
-	// 			// allows overriding page files
-	// 			{
-	// 				source: '/some-page',
-	// 				destination: '/somewhere-else',
-	// 				has: [{ type: 'query', key: 'overrideMe' }]
-	// 			}
-	// 		],
-	// 		afterFiles: [
-	// 			// These rewrites are checked after pages/public files
-	// 			// are checked but before dynamic routes
-	// 			{
-	// 				source: '/non-existent',
-	// 				destination: '/somewhere-else'
-	// 			}
-	// 		],
-	// 		fallback: [
-	// 			// These rewrites are checked after both pages/public files
-	// 			// and dynamic routes are checked
-	// 			{
-	// 				source: '/:path*',
-	// 				destination: `https://my-old-site.com/:path*`,
-	// 			}
-	// 		]
-	// 	}
-	// },
+	async rewrites() {
+		return {
+			beforeFiles: [
+				// These rewrites are checked after headers/redirects
+				// and before all files including _next/public files which
+				// allows overriding page files
+				{
+					source: '/:path*',
+					destination: '/api/:path*'
+				}
+			],
+			// afterFiles: [
+			// 	// These rewrites are checked after pages/public files
+			// 	// are checked but before dynamic routes
+			// 	{
+			// 		source: '/non-existent',
+			// 		destination: '/somewhere-else'
+			// 	}
+			// ],
+			// fallback: [
+			// 	// These rewrites are checked after both pages/public files
+			// 	// and dynamic routes are checked
+			// 	{
+			// 		source: '/:path*',
+			// 		destination: `https://my-old-site.com/:path*`,
+			// 	}
+			// ]
+		}
+	},
 	swcMinify: true
 }
 
