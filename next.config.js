8ee85c3f2e71b5cffe18cfd8a7e1d8e7ee421dc4
Clint.Network <me@clint.network>
2022-08-13 16:34:25 +0200

    Add redirect of index

--- next.config.js
+++ next.config.js
@@ -1,33 +1,31 @@
-/** @type {{rewrites(): Promise<{beforeFiles: [{destination: string, source: string}]}>, reactStrictMode: boolean, swcMinify: boolean}} */
+/**
+ * @type {{rewrites(): Promise<{beforeFiles: [{destination: string, source: string}], afterFiles: [{destination: string, source: string}]}>, redirects(): Promise<[{permanent: boolean, destination: string, source: string}]>, reactStrictMode: boolean, swcMinify: boolean}}
+ */
 const nextConfig = {
 	reactStrictMode: true,
+	async redirects() {
+		return [
+			{
+				source: '/',
+				destination: 'https://hypeproxy.io/ip',
+				permanent: true
+			}
+		]
+	},
 	async rewrites() {
 		return {
 			beforeFiles: [
-				// These rewrites are checked after headers/redirects
-				// and before all files including _next/public files which
-				// allows overriding page files
 				{
-					source: '/:path*',
-					destination: '/api/:path*'
+					source: "/:path*",
+					destination: "/api/:path*"
 				}
 			],
-			// afterFiles: [
-			// 	// These rewrites are checked after pages/public files
-			// 	// are checked but before dynamic routes
-			// 	{
-			// 		source: '/non-existent',
-			// 		destination: '/somewhere-else'
-			// 	}
-			// ],
-			// fallback: [
-			// 	// These rewrites are checked after both pages/public files
-			// 	// and dynamic routes are checked
-			// 	{
-			// 		source: '/:path*',
-			// 		destination: `https://my-old-site.com/:path*`,
-			// 	}
-			// ]
+			afterFiles: [
+				{
+					source: "/ua",
+					destination: "/user-agent"
+				}
+			]
 		}
 	},
 	swcMinify: true
